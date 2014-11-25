e72c45de2f0059e05d90326c3033420ee1e890de
Damian Soriano <damiansoriano@gmail.com>
2014-11-25 22:26:31 +0000

    Initial commit with simple implementation for the scale API

---
+++ hw_scale_latorre/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'HW Scale Latorre',
+    'category': 'HW Scale Latorre',
+    'summary': 'HW Scale Latorre',
+    'version': '1.0',
+    'description': '',
+    'author': 'Damian Soriano <ds@ingadhoc.com>',
+    'depends': ['hw_proxy'],
+    'external_dependencies': {},
+    'data': [],
+    'installable': True,
+}
---
+++ hw_scale_latorre/controllers/main.py
@@ -0,0 +1,44 @@
+import logging
+from threading import Thread
+
+import openerp.addons.hw_proxy.controllers.main as hw_proxy
+from openerp import http
+
+_logger = logging.getLogger(__name__)
+
+try:
+    import serial
+except ImportError:
+    _logger.error('Odoo module hw_scale depends on the pyserial python module')
+    serial = None
+
+
+class ScaleLatorre(Thread):
+
+    def get_weight(self):
+        return '2.3'
+
+    def get_weight_info(self):
+        return 'ok'
+
+
+s = ScaleLatorre()
+
+
+class ScaleDriver(hw_proxy.Proxy):
+
+    @http.route('/hw_proxy/scale_latorre_read/', type='json', auth='none')
+    def scale_read(self):
+        '''
+        This function is accessible if there is only one database installed
+        in the instance.
+
+        You can test this with curl:
+
+        curl -H "Content-Type: application/json" -d '{}' http://localhost:8069/hw_proxy/scale_latorre_read
+        '''
+        return {
+            'weight': s.get_weight(),
+            'unit': 'kg',
+            'info': s.get_weight_info(),
+        }
